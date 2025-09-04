31e323bb68cfe58066698dd69b92a63d3287e9d9
Brett Kettler <68669493+brettmkettler@users.noreply.github.com>
2025-09-04 14:38:59 -0400

    cc

--- run-bms-service.sh
+++ run-bms-service.sh
@@ -3,8 +3,5 @@
 # Wrapper script to run BMS service with full user session context
 # This simulates running the script manually in a terminal
 
-exec su - seanfuchs -c "
-cd /home/seanfuchs/Desktop/j5_console/BtBmsDisplay && 
-source /home/seanfuchs/Desktop/venv/bin/activate && 
-python dual_bms_service.py
-"
+cd /home/seanfuchs/Desktop/j5_console/BtBmsDisplay
+exec su seanfuchs -c "source /home/seanfuchs/Desktop/venv/bin/activate && python dual_bms_service.py"
