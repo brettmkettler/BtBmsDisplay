b0e62e8a614f2b7b117f79d0f0222bb14407c87b
Brett Kettler <68669493+brettmkettler@users.noreply.github.com>
2025-09-05 00:14:29 -0400

    old layout

--- server/index.ts
+++ server/index.ts
@@ -57,10 +57,15 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Default to 3000 to avoid conflict with j5_console.py on port 5000
+  // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
-  const port = parseInt(process.env.PORT || '3000', 10);
-  server.listen(port, "127.0.0.1", () => {
+  // It is the only port that is not firewalled.
+  const port = parseInt(process.env.PORT || '5000', 10);
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
     log(`serving on port ${port}`);
   });
 })();
